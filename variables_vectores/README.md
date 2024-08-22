72d8dda049aafc6c77af018b9d167eedac450a4b
Neftcode <carlos29arevalo@hotmail.com>
2024-08-21 22:13:50 -0500

    repaso de tipos de datos y declaración de variables y vectores

---
+++ variables_vectores/README.md
@@ -0,0 +1,6 @@
+# Contenido de la clase 
+
+- Tipos de dato nativos en programación.
+- Qué es una variable y sus tipos
+- Qué es un vector
+- Cómo declarar variables y vectores
---
+++ variables_vectores/declaracion_variables_y_vectores.py
@@ -0,0 +1,25 @@
+# Ejemplo de declaración de variables
+pera = 5
+pan = 10
+huevo = 30
+carne = 1000
+
+# Ejemplo de como declarar un vector/arreglo de variables
+productos = [pera, pan, huevo, carne]
+# Ejemplo de como declarar un vector de números
+unidades = [5, 10, 30, 1000]
+
+# Ejemplo de declaración de variables
+el_papa_de_alicia = "juan"
+mi_edad = 27
+mi_nombre = "carlos"
+soy_hombre = 1
+mi_amigo = "andrés"
+# Ejemplo de declaración de un arreglo
+mis_amigxs = [mi_amigo, "jhon", "alejandro", "lina"]
+
+
+
+
+
+
