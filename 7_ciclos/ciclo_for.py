dfe158552f216a5d202551ff67f63ab0f18002d2
Neftcode <carlos29arevalo@hotmail.com>
2024-10-16 21:27:50 -0500

    refuerzo ciclo for

--- 7_ciclos/ciclo_for.py
+++ 7_ciclos/ciclo_for.py
@@ -14,3 +14,16 @@ for x in range(1, 101, 1):
         print(f"{x}: El número es par")
     else:
         print(f"{x}: El número es impar")
+
+# Programa que separa un texto según el patrón dado y cuenta el número de oraciones e imprime cada una
+print("Programa que separa un texto según el patrón dado y cuenta el número de oraciones e imprime cada una")
+cadena = "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas \"Letraset\", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum."
+# print(cadena)
+vector_de_oracion = cadena.split('.')
+
+print(f"Total de oraciones: {len(vector_de_oracion)}")
+
+cont = 1
+for oracion in vector_de_oracion:
+    print(f"{cont}. {oracion}")
+    cont+=1
